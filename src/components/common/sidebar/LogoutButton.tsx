083f478b2e7988e095418fe19a8af7c77518f023
gaboe <gabrielecegi@gmail.com>
2018-01-29 22:15:56 +0100

    add logout button

---
+++ src/components/common/sidebar/LogoutButton.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { Menu, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+type Props = {
+  onClick: () => void;
+};
+
+const LogoutButton: React.SFC<Props> = props => {
+  return (
+    <Menu.Item name="logout">
+      <Link
+        to=""
+        onClick={e => {
+          e.preventDefault();
+          props.onClick();
+        }}
+      >
+        <Icon name="gamepad" />
+        Logout
+      </Link>
+    </Menu.Item>
+  );
+};
+
+export { LogoutButton };
--- src/components/common/sidebar/MenuItems.tsx
+++ src/components/common/sidebar/MenuItems.tsx
@@ -1,38 +1,61 @@
 import * as React from "react";
+import { Query } from "react-apollo";
 import { Menu, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-type Props = {};
+import {
+  USER_QUERY as query,
+  Response
+} from "./../../../gql/queries/UserQuery";
+import { Props, withLogout } from "./../../../gql/mutations/users/Logout";
+import { LogoutButton } from "./LogoutButton";
+
+const isAuthentificated = (props: Response) =>
+  props.data !== undefined && props.data.user !== null;
 
 const MenuItems: React.SFC<Props> = props => {
   return (
-    <>
-      <Menu.Item name="home">
-        <Link to="/">
-          <Icon name="home" />
-          Home
-        </Link>
-      </Menu.Item>
-      <Menu.Item name="gamepad">
-        <Link to="/rules">
-          <Icon name="gamepad" />
-          Rules
-        </Link>
-      </Menu.Item>
-      <Menu.Item name="login">
-        <Link to="/login">
-          <Icon name="gamepad" />
-          Login
-        </Link>
-      </Menu.Item>
-      <Menu.Item name="logout">
-        <Link to="/logout">
-          <Icon name="gamepad" />
-          Logout
-        </Link>
-      </Menu.Item>
-    </>
+    <Query query={query}>
+      {(result: Response) => {
+        return (
+          <>
+            <Menu.Item name="home">
+              <Link to="/">
+                <Icon name="home" />
+                Home
+              </Link>
+            </Menu.Item>
+            <Menu.Item name="gamepad">
+              <Link to="/rules">
+                <Icon name="gamepad" />
+                Rules
+              </Link>
+            </Menu.Item>
+
+            {isAuthentificated(result) || (
+              <Menu.Item name="login">
+                <Link to="/login">
+                  <Icon name="gamepad" />
+                  Login
+                </Link>
+              </Menu.Item>
+            )}
+
+            {isAuthentificated(result) && (
+              <LogoutButton
+                onClick={() => {
+                  if (props.mutate) {
+                    props.mutate({ refetchQueries: [{ query: query }] });
+                  }
+                }}
+              />
+            )}
+          </>
+        );
+      }}
+    </Query>
   );
 };
 
-export { MenuItems };
+const hoc = withLogout(MenuItems);
+export { hoc as MenuItems };
