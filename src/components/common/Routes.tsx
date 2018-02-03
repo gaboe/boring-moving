752849acb70e4ae9e8bb719e6ed4cd9de31313cd
gaboe <gabrielecegi@gmail.com>
2018-02-04 00:09:17 +0100

    add AddRule

--- src/components/common/Routes.tsx
+++ src/components/common/Routes.tsx
@@ -5,12 +5,14 @@ import { Home } from "./../home/Home";
 import { Login } from "./../auth/Login";
 import { Register } from "../auth/Register";
 import { RequireAuth } from "./../auth/RequireAuth";
+import { AddRule } from "../rules/AddRule";
 
 const Routes = () => {
   return (
     <div>
       <Route exact={true} path="/" component={Home} />
       <Route path="/rules" component={RequireAuth(Rules)} />
+      <Route path="/add-rule" component={RequireAuth(AddRule)} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
     </div>
--- src/components/rules/Table.tsx
+++ src/components/rules/Table.tsx
@@ -1,33 +1,39 @@
 import * as React from "react";
-import { Table } from "semantic-ui-react";
+import { Table, Button } from "semantic-ui-react";
 import { Rule } from "../../models/Rule";
+import { Link } from "react-router-dom";
 
 type Props = { rules: Rule[] };
 
 const RulesTable: React.SFC<Props> = props => (
-  <Table celled={true}>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Sender</Table.HeaderCell>
-        <Table.HeaderCell>Subject</Table.HeaderCell>
-        <Table.HeaderCell>Content</Table.HeaderCell>
-        <Table.HeaderCell>Period</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+  <>
+    <Link to="/add-rule">
+      <Button color="linkedin">Add rule</Button>
+    </Link>
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Sender</Table.HeaderCell>
+          <Table.HeaderCell>Subject</Table.HeaderCell>
+          <Table.HeaderCell>Content</Table.HeaderCell>
+          <Table.HeaderCell>Period</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
 
-    <Table.Body>
-      {props.rules.map(x => {
-        return (
-          <Table.Row key={x.id}>
-            <Table.Cell>{x.sender}</Table.Cell>
-            <Table.Cell>{x.subject}</Table.Cell>
-            <Table.Cell>{x.content}</Table.Cell>
-            <Table.Cell>{x.period}</Table.Cell>
-          </Table.Row>
-        );
-      })}
-    </Table.Body>
-  </Table>
+      <Table.Body>
+        {props.rules.map(x => {
+          return (
+            <Table.Row key={x.id}>
+              <Table.Cell>{x.sender}</Table.Cell>
+              <Table.Cell>{x.subject}</Table.Cell>
+              <Table.Cell>{x.content}</Table.Cell>
+              <Table.Cell>{x.period}</Table.Cell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table>
+  </>
 );
 
 export { RulesTable as Table };
