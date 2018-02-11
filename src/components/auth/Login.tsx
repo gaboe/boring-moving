8eb2cc2382c6454c49711369cf72da8177056429
gaboe <gabrielecegi@gmail.com>
2018-02-11 14:27:29 +0100

    add login with google button

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,19 +1,14 @@
 import * as React from "react";
-import {
-  Button,
-  Form,
-  InputOnChangeData,
-  Message,
-  Grid
-} from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import { Form, InputOnChangeData, Grid } from "semantic-ui-react";
 
 import { withLoginMutation, Props } from "./../../gql/mutations/users/Login";
-import { nameof } from "./../../utils/Reflection";
 import { USER_QUERY } from "./../../gql/queries/UserQuery";
 import { RouteComponentProps } from "react-router";
 import GridColumn from "semantic-ui-react/dist/commonjs/collections/Grid/GridColumn";
+import { GoogleLogin, GoogleLoginResponse } from "react-google-login";
+import { CSSProperties } from "react";
 
+const GoogleButtonStyle: CSSProperties = {};
 type State = {
   email: string;
   password: string;
@@ -47,38 +42,24 @@ class Login extends React.Component<PropsWithRouter, State> {
     }
   };
 
+  googleResponse = (response: GoogleLoginResponse) => {
+    console.log(response);
+  };
+
   render() {
     return (
       <Grid columns="12">
         <GridColumn width={4} />
         <GridColumn width={8}>
           <Form error={this.state.formError}>
-            <Form.Input
-              name={nameof<State>("email")}
-              label="Email"
-              type="input"
-              placeholder="email"
-              onChange={this.handleChange}
+            <GoogleLogin
+              className="ui linkedin button"
+              clientId="578678813391-3khelub231ejgukuui1r4dqkg67o8p39.apps.googleusercontent.com"
+              buttonText="Login with Google"
+              onSuccess={this.googleResponse}
+              onFailure={this.googleResponse}
+              style={GoogleButtonStyle}
             />
-            <Form.Input
-              name={nameof<State>("password")}
-              label="Password"
-              type="password"
-              placeholder="password"
-              onChange={this.handleChange}
-            />
-            <Message
-              error={true}
-              header="Ups"
-              content={`It seem's like your credentials are not correct, 
-                        if you are not registered, you can register here`}
-            />
-            <Button color="linkedin" onClick={this.login} type="submit">
-              Submit
-            </Button>
-            <Link to="/register">
-              <Button>Register</Button>
-            </Link>
           </Form>
         </GridColumn>
         <GridColumn width={1} />
