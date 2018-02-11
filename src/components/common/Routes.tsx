50928b36d2ddf698518df2c056f64bd762717efd
gaboe <gabrielecegi@gmail.com>
2018-02-11 20:18:59 +0100

    hide registration

--- src/components/common/Routes.tsx
+++ src/components/common/Routes.tsx
@@ -3,7 +3,6 @@ import { Route } from "react-router-dom";
 import { Rules } from "./../rules/Rules";
 import { Home } from "./../home/Home";
 import { Login } from "./../auth/Login";
-import { Register } from "../auth/Register";
 import { RequireAuth } from "./../auth/RequireAuth";
 import { AddRule } from "../rules/AddRule";
 import { UserSettings } from "../userSettings/UserSettings";
@@ -15,7 +14,7 @@ const Routes = () => {
       <Route path="/rules" component={RequireAuth(Rules)} />
       <Route path="/add-rule" component={RequireAuth(AddRule)} />
       <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
+      {/* <Route path="/register" component={Register} /> */}
       <Route path="/user-settings" component={RequireAuth(UserSettings)} />
     </div>
   );
