79f2272a5ff992a94045d026887febab9bcfe25d
gaboe <gabrielecegi@gmail.com>
2018-02-25 19:05:53 +0100

    add basic fake chart

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Query } from "react-apollo";
 import { USER_QUERY as query, Props } from "./../../gql/queries/UserQuery";
+import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
 
 const getName = (result: Props): string => {
   if (result.data && result.data.user) {
@@ -10,10 +11,38 @@ const getName = (result: Props): string => {
 };
 
 const Home: React.SFC<Props> = props => {
+  const data = [
+    {
+      value: 3400,
+      name: "Page A"
+    },
+    {
+      value: 2400,
+      name: "Page B"
+    },
+    {
+      value: 5400,
+      name: "Page C"
+    },
+    {
+      value: 8400,
+      name: "Page D"
+    }
+  ];
   return (
     <Query query={query}>
       {(result: Props) => {
-        return <div>Hello {getName(result)}!</div>;
+        return (
+          <>
+            <div>Hello {getName(result)}!</div>
+            <LineChart width={800} height={400} data={data}>
+              <Line type="monotone" dataKey="value" stroke="#8884d8" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+            </LineChart>
+          </>
+        );
       }}
     </Query>
   );
