0ba1a70e387d682d179d9c1e631637248fd04a56
gaboe <gabrielecegi@gmail.com>
2018-03-04 00:11:32 +0100

    add responsivity to home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -8,53 +8,96 @@ import { MostActiveRulesQueryVariables } from "../../generated/types";
 import { Header } from "semantic-ui-react";
 import { Row, Col } from "react-grid-system";
 
-const Home: React.SFC<{}> = props => {
-  const variables: MostActiveRulesQueryVariables = { count: 5 };
-  return (
-    <MostActiveQueryComponent
-      query={MOST_ACTIVE_RULES_QUERY}
-      variables={variables}
-    >
-      {result => {
-        if (result.loading) {
-          return null;
-        }
-        if (result.data) {
+type State = {
+  windowHeight: number;
+  windowWidth: number;
+
+}
+
+class Home extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { windowHeight: window.innerHeight, windowWidth: window.innerWidth }
+  }
+
+  handleResize = () => {
+    this.setState({
+      windowHeight: window.innerHeight,
+      windowWidth: window.innerWidth
+    });
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize)
+  }
+
+  getWidth = () => {
+    if (this.state.windowWidth < 1000) {
+      return this.state.windowWidth - 100;
+    }
+    if (this.state.windowWidth < 1500) {
+      return 600;
+    }
+    return 800;
+  }
+
+  render() {
+    const variables: MostActiveRulesQueryVariables = { count: 5 };
+    return (
+      <MostActiveQueryComponent
+        query={MOST_ACTIVE_RULES_QUERY}
+        variables={variables}
+      >
+        {result => {
+          if (result.loading) {
+            return null;
+          }
+          if (result.data) {
+            return (
+              <>
+                <Row>
+                  <Col offset={{ sm: 5 }} sm={2}>
+                    <Header
+                      as="h3"
+                      content={`Boring moving moved ${
+                        result.data.mostActiveRules.count
+                        } emails`}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col offset={{ sm: 1, lg: 2, xl: 3 }} sm={10} lg={8} xl={6}>
+
+                    <BarChart
+                      width={this.getWidth()}
+                      height={this.state.windowHeight - 100}
+                      data={result.data.mostActiveRules.rules.map(x => {
+                        return { rule: `${x.rule.folderName}`, count: x.count };
+                      })}
+                    >
+                      <XAxis dataKey="rule" />
+                      <YAxis />
+                      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                      <Bar dataKey="count" fill="#016936" barSize={30} />
+                    </BarChart>
+                  </Col>
+                </Row>
+              </>
+            );
+          }
           return (
-            <>
-              <Row>
-                <Col offset={{ sm: 1, lg: 3 }} sm={10} lg={6}>
-                  <Header
-                    as="h3"
-                    content={`Boring moving moved ${
-                      result.data.mostActiveRules.count
-                      } emails`}
-                  />
-                  <BarChart
-                    width={900}
-                    height={600}
-                    data={result.data.mostActiveRules.rules.map(x => {
-                      return { rule: `${x.rule.folderName}`, count: x.count };
-                    })}
-                  >
-                    <XAxis dataKey="rule" />
-                    <YAxis />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <Bar dataKey="count" fill="#016936" barSize={30} />
-                  </BarChart>
-                </Col>
-              </Row>
-            </>
+            <Header
+              as="h3"
+              content="There are no data about moved emails. Have you configured imap and created rule?"
+            />
           );
-        }
-        return (
-          <Header
-            as="h3"
-            content="There are no data about moved emails. Have you configured imap and created rule?"
-          />
-        );
-      }}
-    </MostActiveQueryComponent>
-  );
-};
+        }}
+      </MostActiveQueryComponent>
+    );
+  };
+}
 export { Home };
