95d202f63b182b8be08cb1618a038b7735b128f2
gaboe <gabrielecegi@gmail.com>
2018-03-03 21:40:29 +0100

    add AppEmailCount component

---
+++ src/components/auth/AppEmailCount.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { AppStatQueryComponent, APP_STAT_QUERY } from "../../gql/queries/stats/AppStatQuery";
+import { Statistic, Icon } from "semantic-ui-react";
+import { Col, Row, } from 'react-grid-system';
+
+class AppEmailCount extends React.Component<{}> {
+    render() {
+        return (
+            <>
+                <AppStatQueryComponent query={APP_STAT_QUERY} >
+                    {
+                        response => {
+                            if (!response.data) {
+                                return null;
+                            }
+                            return (
+                                <>
+                                    <Row >
+                                        <Col offset={{ xs: 5, lg: 5 }}>
+                                            <Statistic size="huge">
+                                                <Statistic.Value>
+                                                    <Icon name='mail' />
+                                                    {response.data.appStat.emailCount}
+                                                </Statistic.Value>
+                                                <Statistic.Label>Emails moved by Boring Moving!</Statistic.Label>
+                                            </Statistic>
+                                        </Col>
+                                    </Row>
+                                </>
+                            )
+                        }
+                    }
+
+                </AppStatQueryComponent >
+
+            </>
+        );
+    }
+}
+
+export { AppEmailCount }
--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { InputOnChangeData, Header, Statistic, Icon } from "semantic-ui-react";
+import { InputOnChangeData, Header, } from "semantic-ui-react";
 import { withLoginMutation, Props } from "./../../gql/mutations/users/Login";
 import { USER_QUERY } from "./../../gql/queries/UserQuery";
 import { RouteComponentProps } from "react-router";
@@ -7,7 +7,7 @@ import { GoogleLogin, GoogleLoginResponse } from "react-google-login";
 import { CSSProperties } from "react";
 import { NonAuthenificatedUser } from "../../models/User";
 import { Col, Row, } from 'react-grid-system';
-import { AppStatQueryComponent, APP_STAT_QUERY } from "../../gql/queries/stats/AppStatQuery";
+import { AppEmailCount } from "./AppEmailCount";
 
 const GoogleButtonStyle: CSSProperties = {};
 type State = {
@@ -63,49 +63,26 @@ class Login extends React.Component<PropsWithRouter, State> {
   render() {
     return (
       <>
-        <AppStatQueryComponent query={APP_STAT_QUERY} >
-          {
-            response => {
-              return (
-                <>
-                  <Row >
-                    <Col offset={{ xs: 1, sm: 1, md: 2, lg: 3 }} xs={10} lg={6}>
-                      <Header as="h1" content="Welcome to Boring Moving!" />
-                      <Header as="h2" content="App that automates boring email moving" />
+        <Row >
+          <Col offset={{ xs: 1, sm: 1, md: 2, lg: 3 }} xs={10} lg={6}>
+            <Header as="h1" content="Welcome to Boring Moving!" />
+            <Header as="h2" content="App that automates boring email moving" />
 
-                      <Header as="h3">
-                        In order to connect to google API, you need to login with your
-                        Google account
-                      </Header>
-
-                      <GoogleLogin
-                        className="ui linkedin button"
-                        clientId="578678813391-3khelub231ejgukuui1r4dqkg67o8p39.apps.googleusercontent.com"
-                        buttonText="Login with Google"
-                        onSuccess={this.googleResponse}
-                        onFailure={this.googleResponse}
-                        style={GoogleButtonStyle}
-                      />
-                    </Col>
-                  </Row >
-                  <Row >
-                    <Col offset={{ xs: 5, lg: 5 }}>
-                      <Statistic size="huge">
-                        <Statistic.Value>
-                          <Icon name='mail' />
-                          {response.data != null ? ` ${response.data.appStat.emailCount} ` : " many "}
-                        </Statistic.Value>
-                        <Statistic.Label>Emails moved by Boring Moving!</Statistic.Label>
-                      </Statistic>
-                    </Col>
-                  </Row>
-                </>
-              )
-            }
-          }
-
-        </AppStatQueryComponent >
-
+            <Header as="h3">
+              In order to connect to google API, you need to login with your
+              Google account
+            </Header>
+            <GoogleLogin
+              className="ui linkedin button"
+              clientId="578678813391-3khelub231ejgukuui1r4dqkg67o8p39.apps.googleusercontent.com"
+              buttonText="Login with Google"
+              onSuccess={this.googleResponse}
+              onFailure={this.googleResponse}
+              style={GoogleButtonStyle}
+            />
+          </Col>
+        </Row >
+        <AppEmailCount />
       </>
     );
   }
