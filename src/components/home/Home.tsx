89ad442fdab9c3a5730d061fb2eca01515e8e274
gaboe <gabrielecegi@gmail.com>
2018-03-03 23:26:52 +0100

    add cols and rows to home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -6,6 +6,7 @@ import {
 } from "../../gql/queries/rules/MostActiveRulesQuery";
 import { MostActiveRulesQueryVariables } from "../../generated/types";
 import { Header } from "semantic-ui-react";
+import { Row, Col } from "react-grid-system";
 
 const Home: React.SFC<{}> = props => {
   const variables: MostActiveRulesQueryVariables = { count: 5 };
@@ -21,24 +22,28 @@ const Home: React.SFC<{}> = props => {
         if (result.data) {
           return (
             <>
-              <Header
-                as="h3"
-                content={`Boring moving moved ${
-                  result.data.mostActiveRules.count
-                } emails`}
-              />
-              <BarChart
-                width={900}
-                height={600}
-                data={result.data.mostActiveRules.rules.map(x => {
-                  return { rule: `${x.rule.folderName}`, count: x.count };
-                })}
-              >
-                <XAxis dataKey="rule" />
-                <YAxis />
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <Bar dataKey="count" fill="#016936" barSize={30} />
-              </BarChart>
+              <Row>
+                <Col offset={{ sm: 1, lg: 3 }} sm={10} lg={6}>
+                  <Header
+                    as="h3"
+                    content={`Boring moving moved ${
+                      result.data.mostActiveRules.count
+                      } emails`}
+                  />
+                  <BarChart
+                    width={900}
+                    height={600}
+                    data={result.data.mostActiveRules.rules.map(x => {
+                      return { rule: `${x.rule.folderName}`, count: x.count };
+                    })}
+                  >
+                    <XAxis dataKey="rule" />
+                    <YAxis />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <Bar dataKey="count" fill="#016936" barSize={30} />
+                  </BarChart>
+                </Col>
+              </Row>
             </>
           );
         }
