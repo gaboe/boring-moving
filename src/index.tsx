eff0553096930d53e8f5990035f3a350975a9e20
gaboe <gabrielecegi@gmail.com>
2018-03-08 00:03:37 +0100

    unregister service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
-
+import { unregister as unregisterServiceWorker } from './registerServiceWorker'
 ReactDOM.render(
   <App />,
   document.getElementById("root") as HTMLElement
 );
-registerServiceWorker();
+unregisterServiceWorker();
