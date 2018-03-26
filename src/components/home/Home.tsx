faf69f3a33aaafb987d5605e58e0e4d5a19c4ee1
gaboe <gabrielecegi@gmail.com>
2018-03-26 21:45:44 +0200

    Imporve chart

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -7,12 +7,17 @@ import {
 import { MostActiveRulesQueryVariables } from "../../generated/types";
 import { Header } from "semantic-ui-react";
 import { Row, Col } from "react-grid-system";
+import styled from "styled-components";
 
 type State = {
   windowHeight: number;
   windowWidth: number;
-
 }
+
+const MovedData = styled.div`
+  margin-top: 1em;
+  margin-bottom: 2em;
+`
 
 class Home extends React.Component<{}, State> {
   constructor(props: {}) {
@@ -36,13 +41,13 @@ class Home extends React.Component<{}, State> {
   }
 
   getWidth = () => {
-    if (this.state.windowWidth < 1000) {
-      return this.state.windowWidth - 100;
+    console.log(this.state.windowWidth);
+
+    if (this.state.windowWidth < 700) {
+      return 400;
     }
-    if (this.state.windowWidth < 1500) {
-      return 600;
-    }
-    return 800;
+
+    return this.state.windowWidth - 250;
   }
 
   render() {
@@ -59,27 +64,30 @@ class Home extends React.Component<{}, State> {
           if (result.data.mostActiveRules.count > 0) {
             return (
               <>
+                <MovedData>
+                  <Row>
+                    <Col offset={{ xs: 2, sm: 3, }} xs={8} sm={4}>
+                      <Header
+                        as="h3"
+                        content={`Boring moving moved ${
+                          result.data.mostActiveRules.count
+                          } emails`}
+                      />
+                    </Col>
+                  </Row>
+                </MovedData>
                 <Row>
-                  <Col offset={{ xs: 2, sm: 4, }} xs={8} sm={4}>
-                    <Header
-                      as="h3"
-                      content={`Boring moving moved ${
-                        result.data.mostActiveRules.count
-                        } emails`}
-                    />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col offset={{ sm: 1, lg: 2, xl: 3 }} sm={10} lg={8} xl={6}>
+                  <Col>
                     <BarChart
+                      layout="vertical"
                       width={this.getWidth()}
                       height={this.state.windowHeight < 600 ? this.state.windowHeight - 100 : 600}
                       data={result.data.mostActiveRules.rules.map(x => {
                         return { rule: `${x.rule.subject ? x.rule.subject : x.rule.folderName}`, count: x.count };
                       })}
                     >
-                      <XAxis dataKey="rule" />
-                      <YAxis />
+                      <XAxis type="number" />
+                      <YAxis type="category" dataKey="rule" />
                       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                       <Bar dataKey="count" fill="#016936" barSize={30} />
                     </BarChart>
